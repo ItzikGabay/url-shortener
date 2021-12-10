0d76b6c0811523f509e0ffdee67ecd8c852a2671
ItzikDevio <itzikdevio@gmail.com>
2021-12-10 23:44:56 +0200

    Added styled Navbar component

---
+++ client/components/navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import css from './navbar.module.css';
+
+export const Navbar: React.FC = () => {
+  return (
+    <nav className={css['nav-container']}>
+      <div className={css['navbar-left']}>
+        <p>URL Shortener</p>
+      </div>
+      <div className={css['navbar-right']}>
+        <ul className={css['navbar-links']}>
+          <li>Click me</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
