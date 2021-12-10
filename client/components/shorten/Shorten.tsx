9eaff9b8ef0c5b12e598dfd19f929ca8bf68ff07
ItzikDevio <itzikdevio@gmail.com>
2021-12-11 01:56:05 +0200

    Added basic logic to the home component

--- client/components/shorten/Shorten.tsx
+++ client/components/shorten/Shorten.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import css from './shorten.module.css';
 
 export interface ShortenProps {}
 
 export const Shorten: React.FC<ShortenProps> = ({}) => {
+  // const linkInput: any = useRef(null);
+  const [linkInput, setLinkInput] = useState('');
+
+  const buttonHandler = () => {
+
+
+    setLinkInput('RESULT FROM THE API');
+  };
+
   return (
     <div className={css['shorten-container']}>
       <div className={css['shorten-link']}>
-        <input type="text" id="link" placeholder="Insert link to shorten" />
-        <button>lala</button>
+        <input
+          type="text"
+          id="link"
+          placeholder="Insert link to shorten"
+          value={linkInput}
+          onChange={(event) => setLinkInput(event.target.value)}
+        />
+        <button onClick={() => buttonHandler()}>Generate your link</button>
       </div>
     </div>
   );
