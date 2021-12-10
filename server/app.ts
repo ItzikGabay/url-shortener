31969091ecb5b4219bd9b24f9e546dcbcb1b9cb4
ItzikDevio <itzikdevio@gmail.com>
2021-12-10 23:02:08 +0200

    Added basic express configuration

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,13 @@
 import express, { Express, Request, Response, NextFunction } from 'express';
 import RouterHandler from './routes/index';
+import cors from 'cors';
 
 const app: Express = express();
-const port = 5000;
+const port: number = 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: 'http://localhost/' }));
 
 app.use('/', RouterHandler);
 
