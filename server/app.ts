342188443a1f9f2c8fe271673f2c77b84961306c
ItzikDevio <itzikdevio@gmail.com>
2021-12-10 22:35:36 +0200

    Added Nextjs as the client

--- server/app.ts
+++ server/app.ts
@@ -3,8 +3,6 @@ import express, { Express, Request, Response, NextFunction } from 'express';
 const app: Express = express();
 const port = 3000;
 
-
-
 app.listen(port, () => {
   console.log(`url-shortener application is running on port ${port}.`);
 });
