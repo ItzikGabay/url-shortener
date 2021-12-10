9158b2343f72eaec4a84d50a134ac61bd20587c2
ItzikDevio <itzikdevio@gmail.com>
2021-12-10 22:42:50 +0200

    Added basic configuration

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response, NextFunction } from 'express';
 
 const app: Express = express();
-const port = 3000;
+const port = 5000;
 
 app.listen(port, () => {
   console.log(`url-shortener application is running on port ${port}.`);
