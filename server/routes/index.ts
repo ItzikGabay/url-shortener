11667d2f5e8264c09686b76f9ca9f6fd57e177db
ItzikDevio <itzikdevio@gmail.com>
2021-12-11 02:03:11 +0200

    Added basic BE logic

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -10,4 +10,19 @@ RouterHandler.route('/')
     res.send('POST: works');
   });
 
+RouterHandler.route('/shorten').post((req, res) => {
+  const result = `${req.body.url} is saved as 'shorten.io/${generateUID()}'`;
+  res.send(result);
+});
+
 export default RouterHandler;
+
+// I generate the UID from two parts here
+// to ensure the random number provide enough bits.
+function generateUID() {
+  let firstPart: number | string = (Math.random() * 46656) | 0;
+  let secondPart: number | string = (Math.random() * 46656) | 0;
+  firstPart = ('000' + firstPart.toString(36)).slice(-3);
+  secondPart = ('000' + secondPart.toString(36)).slice(-3);
+  return firstPart + secondPart;
+}
